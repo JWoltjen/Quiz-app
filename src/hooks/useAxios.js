36e1d5c42392a45227868889097cba3b27068294
JWoltjen <Jeff.Woltjen@gmail.com>
2021-11-02 14:40:46 -0500

    debug useAxios custom hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,5 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
+
+axios.defaults.baseURL = 'https://opentdb.com/'
 
 function useAxios({ url }) {
     const [response, setResponse] = useState(null); 
@@ -14,9 +16,10 @@ function useAxios({ url }) {
             .catch(err => setError(err))
             .finally(() => setLoading(false))
         }
+        fetchData(); 
     }, [url]);
 
-return {response, error, loading}
+    return {response, error, loading}
 
 }
 
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Button } from '@mui/material'
 import { Box } from '@mui/system'
 import { SelectField } from '../components/SelectField'
+import useAxios from '../hooks/useAxios'
 
 function Settings() {
-
+    const { response, error, loading } = useAxios({ url: "/api_category.php"})
+    console.log(response)
     const handleSubmit = e => {
         e.preventDefault(); 
     }
-    
+
     return (
         <form onSubmit={handleSubmit}>
             <SelectField label="Category"/>
