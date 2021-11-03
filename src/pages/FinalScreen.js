acbb2da3f3b756d5fc7031b845464f25257273d5
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-03 14:57:52 -0500

    add state reset to go back to start

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { handleAmountChange, handleScoreChange } from "../redux/actions";
 
-function FinalScreen() {
-    return (
-        <div>
-           Final Screen 
-        </div>
-    )
-}
+const FinalScreen = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { score } = useSelector((state) => state);
 
-export default FinalScreen
+  const handleBackToSettings = () => {
+    dispatch(handleScoreChange(0));
+    dispatch(handleAmountChange(50));
+    history.push("/");
+  };
+
+  return (
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Final Score {score}
+      </Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">
+        Play again? 
+      </Button>
+    </Box>
+  );
+};
+
+export default FinalScreen;
